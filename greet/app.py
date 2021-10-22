9cb7aaf8aa8ddc2a350be05b9954df8df4844a66
Cynthia Canada <cynthiazcanada@gmail.com>
2021-10-22 13:05:35 -0500

    greetings completed

--- greet/app.py
+++ greet/app.py
@@ -6,19 +6,31 @@ app = Flask(__name__)
 @app.route('/welcome')
 def welcome():
     """Return 'welcome' greeting"""
-    html = '<html><body><h1>welcome</h1></body></html>'
-    return html
+    return """
+    <html>
+      <body>
+        <h1>welcome</h1>
+      </body>
+    </html>"""
 
 
 @app.route('/welcome/home')
 def welcome_home():
     """Return 'welcome home' greeting"""
-    html = '<html><body><h1>welcome home</h1></body></html>'
-    return html
+    return """
+    <html>
+      <body>
+        <h1>welcome home</h1>
+      </body>
+    </html>"""
 
 
 @app.route('/welcome/back')
 def welcome_back():
     """Return 'welcome back' greeting"""
-    html = '<html><body><h1>welcome back</h1></body></html>'
-    return html
+    return """
+    <html>
+      <body>
+        <h1>welcome back</h1>
+      </body>
+    </html>"""
