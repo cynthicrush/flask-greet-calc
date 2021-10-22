b7cf14e881e6b3bb8f8ab7fffd2346c22e49b6e5
Cynthia Canada <cynthiazcanada@gmail.com>
2021-10-22 12:27:25 -0500

    functions added

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,22 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+
+@app.route('/add')
+def add():
+    """Adding a and b"""
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = add(a, b)
+    return str(result)
+
+@app.route('/sub')
+def subtract():
+    """Subtracting a and b"""
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = sub(a, b)
+    return str(result)
