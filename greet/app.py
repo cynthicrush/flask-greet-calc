6b06e818587d4999d65b288bc94d4dd43a624c6b
Cynthia Canada <cynthiazcanada@gmail.com>
2021-10-22 13:00:37 -0500

    Greetings completed

---
+++ greet/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/welcome')
+def welcome():
+    """Return 'welcome' greeting"""
+    html = '<html><body><h1>welcome</h1></body></html>'
+    return html
+
+
+@app.route('/welcome/home')
+def welcome_home():
+    """Return 'welcome home' greeting"""
+    html = '<html><body><h1>welcome home</h1></body></html>'
+    return html
+
+
+@app.route('/welcome/back')
+def welcome_back():
+    """Return 'welcome back' greeting"""
+    html = '<html><body><h1>welcome back</h1></body></html>'
+    return html
